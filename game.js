373dd0c38a2a67965454f8056c751279e0fd74f1
Don Davis <dcaa2005@hotmail.com>
2018-08-04 19:38:05 -0600

    Added basic js functions

--- game.js
+++ game.js
@@ -1,13 +1,49 @@
 var health = 100;
+var numAmraams = 2;
+var numSidewinders = 4;
+var numRounds = 6000;
 
 const advHealth = document.getElementById("health");
-function draw() {
-  advHealth.innerText = health.toString();
+const rounds = document.getElementById("rounds");
+const sidewinders = document.getElementById("sidewinders");
+const amraam = document.getElementById("amraam");
+
+const gunButton = document.getElementById("gun-button");
+const sidewinderButton = document.getElementById("sidewinder-button");
+const amraamButton = document.getElementById("amraam-button");
+
+function fireCannon() {
+  health -= 2;
+  numRounds -= 100;
+  if (numRounds <= 0) {
+    gunButton.setAttribute('disabled', 'true');
+  }
+  draw();
 }
 
-function attack(damage) {
-  health -= damage;
+function launchSidewinder() {
+  health -= 10;
+  numSidewinders -= 1;
+  if (numSidewinders <= 0) {
+    sidewinderButton.setAttribute('disabled', 'true');
+  }
+  draw()
+}
+
+function launchAmraam() {
+  health -= 20;
+  numAmraams -= 1;
+  if (numAmraams <= 0) {
+    amraamButton.setAttribute('disabled', 'true');
+  }
   draw();
+}
+
+function draw() {
+  advHealth.innerText = health.toString();
+  rounds.innerText = numRounds.toString();
+  sidewinders.innerText = numSidewinders.toString();
+  amraam.innerText = numAmraams.toString();
 }
 
 draw();
